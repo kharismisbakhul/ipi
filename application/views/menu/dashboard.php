87e08630b61af8670ef8b12e63a5ecdb00be0cae
Misbakhul Kharis <kharismisbakhul@gmail.com>
2019-11-24 21:42:24 +0700

    Header Dashboard, Change Password
    Penambahan header dan ubah password

--- application/views/menu/dashboard.php
+++ application/views/menu/dashboard.php
@@ -1,6 +1,40 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
-  <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+
+  <!-- Page Heading -->
+  <div class="d-sm-flex align-items-center justify-content-between mb-3">
+    <h1 class="h2 mb-0 text-gray-800"><?= $title;  ?></h1>
+    <div class="tanggal">
+      <div class="text-s mb-0 font-weight-bold text-gray-400">
+        <span><i class="fas fa-calendar-day text-gray-400"></i></span> <?= date('d M Y') ?>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="row">
+    <!-- //Updated -->
+    <div class="col-lg-12">
+      <?= $this->session->flashdata('message');  ?>
+    </div>
+    <div class="d-none d-lg-block col-md-12 mb-4 box2">
+      <div class="card shadow py-0">
+        <div class="card-body dash">
+          <div class="row no-gutters align-items-center">
+            <div class="col ml-auto ">
+              <div class="h5 mb-0 font-weight-normal">Hai <span id="user_name" class="font-weight-bold text-capitalize"><?= $username ?></span>, Selamat datang di IPI - Apps</div>
+              <div class="text-s font-weight-normal text-gray-800 mt-2"></div>
+              <small>Untuk mendapatkan pengalaman yang lebih dalam menggunakan Ipi Apps <br>diharapakan menggunakan browser mozilla firefox, google chrome atau opera</small>
+            </div>
+            <div class="col-auto mr-3">
+              <img src="<?= base_url('assets/img/ad.png') ?>" style="width: 80px">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <div class="row mt-4">
     <div class="col-lg-12 col-md-12 col-sm-12 box2">
       <div class="card shadow h-100">
--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -16,7 +16,7 @@
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small kode_user" id="kode_user" value="<?= $this->session->userdata('status_user') ?>"></span>
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small kode_user" id="kode_user" value="<?= $this->session->userdata('status_user') ?>"><?= $username; ?></span>
                         <img class="img-profile rounded-circle" src="<?= base_url('assets/img/login_page.svg') ?>">
                     </a>
                     <!-- Dropdown - User Information -->
@@ -25,7 +25,7 @@
                             <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i>
                             Ganti Password
                         </a>
-                        <a class="dropdown-item logout" href="<?= base_url('auth/logout') ?>">
+                        <a class="dropdown-item logout" href="#">
                             <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                             Logout
                         </a>
